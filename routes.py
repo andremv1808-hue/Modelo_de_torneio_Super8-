59f136974dada341741656d5b531d71dddd4c290
Andre Marschal Vieira <andremv1808@gmail.com>
2026-03-15 20:30:02 -0300

    Integrei com o framework flask;
    agora as variaveis recebem valor pelo html atraves de funcoes

---
+++ routes.py
@@ -0,0 +1,21 @@
+from main import app, workbook, wb
+from flask import render_template, request
+
+
+@app.route("/")
+def home_page():
+    return render_template("home_page.html")
+
+@app.route("/receber", methods=['POST'])
+
+def receber():
+    p1 = workbook['B3'] = request.form.get('atleta1')
+    p2 = workbook['B4'] = request.form.get('atleta2')
+    p3 = workbook['B5'] = request.form.get('atleta3')
+    p4 = workbook['B7'] = request.form.get('atleta4')
+    p5 = workbook['B8'] = request.form.get('atleta5')
+    p6 = workbook['B6'] = request.form.get('atleta6')
+    p7 = workbook['B9'] = request.form.get('atleta7')
+    p8 = workbook['B10'] =request.form.get('atleta8')
+    wb.save(filename='Pasta1teste.xlsx')
+    return (f"foi {p1} {p2} {p3} {p4} {p5} {p6} {p7} {p8}")
