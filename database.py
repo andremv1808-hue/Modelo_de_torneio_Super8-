85a149f4995f545430e1bec4a5199cc3e361d0db
Andre Marschal Vieira <andremv1808@gmail.com>
2026-07-07 19:52:52 -0300

    AJUSTE NA TABELA DE JOGOS PARA SALVAR OS RESULTADOS DE CADA PARTIDA

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import sqlite3
 
 
+
+
 def connect_db():
-    database = sqlite3.connect("jogadores.db")
+    database = sqlite3.connect("database.db")
     return database
 
 def init_db():
@@ -10,6 +12,7 @@ def init_db():
     database = connect_db()
     cursor = database.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS jogadores (id text,nome text, pontos integer, vitorias integer)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS partidas (id text, j1time1 text,j2time1 text, j1time2 text,j2time2 text, result_time1 integer, result_time2 interger, status text)")
     database.commit()
 
 
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def reqplayers():
     database = connect_db()
     database.execute("DELETE FROM jogadores")
+    database.execute("DELETE FROM partidas")
     database.commit()
     return render_template("reqplayers.html")
 
@@ -21,6 +22,10 @@ def super8():
     cursor = database.cursor()
     just_to_fill = ["A definir","A definir","A definir","A definir","A definir","A definir","A definir","A definir"]
     just_to_fill2 = [0,0,0,0,0,0,0,0]
+    just_to_fill3 = [0,0,0,0,0,0,0,0]
+    results = [0,0,0,0,0,0,0,0]
+    results_on_data = []
+    status = ["False","False","False","False","False","False","False","False","False","False","False","False","False","False",]
 
 
     player = []
@@ -38,6 +43,8 @@ def super8():
         i = i +1
         database.execute("INSERT INTO jogadores (id, nome) VALUES (?,?)", (str(i),players,))
         database.commit()
+    
+
 
     database.execute("UPDATE jogadores SET pontos = ?", (0,))
     database.execute("UPDATE jogadores SET vitorias = ?", (0,))
@@ -46,7 +53,7 @@ def super8():
     cursor.execute("SELECT pontos FROM jogadores")
     rank_pontos = cursor.fetchall()
 
-    return render_template("torneio.html", player=player, rank_pontos = rank_pontos[0], rank_jogadores = just_to_fill, rank_vitorias = just_to_fill2)
+    return render_template("torneio.html", player=player, rank_pontos = just_to_fill3, rank_jogadores = just_to_fill, rank_vitorias = just_to_fill2, results_on_data = results_on_data, status = status)
  
 
 
@@ -56,6 +63,7 @@ def super8():
 def func_button():
     database = connect_db()
     database.execute("DELETE FROM jogadores")
+    database.execute("DELETE FROM partidas")
     database.commit()
 
     return redirect(url_for('reqplayers'))
@@ -67,7 +75,6 @@ def func_button():
 def button_refresh():
     database = connect_db()
     cursor = database.cursor()
-
 # RETORNANDO OS PLAYERS DO BANCO DE DADOS PARA QUE O HTML NAO PERCA ESTES DADOS 
     player = []
     cursor.execute("SELECT nome FROM jogadores")
@@ -93,31 +100,26 @@ def button_refresh():
 
     hidden_ids = novas_sublistas
 
+# AJUSTA A TABELA DE PARTIDAS COM NOMES APENAS UMA VEZ
+    cursor.execute("SELECT EXISTS(SELECT 1 FROM partidas)")
+    tem_data = cursor.fetchone()[0]
+    if not tem_data:
+        for i in range(0,28,2):
+            cursor.execute("INSERT INTO partidas (id, j1time1, j2time1, j1time2, j2time2, result_time1, result_time2, status) VALUES (?,?,?,?,?,?,?,?)", (str(i//2 + 1),hidden_ids[i][0], hidden_ids[i][1], hidden_ids[i + 1][0], hidden_ids[i + 1][1], 0, 0, "False"))
+    database.commit()
+
+
 # ATUALIZA O BANCO DE DADOS COM A SOMA DA VITORIA DE CADA UM, SE FOR EMPATE NAO SOMA NADA
-    ''' 
-    Temos a lista de resultados que retorna 28 numeros, todos de forma padrao de lista [] sem ser separado por outra lista
-
-    Temos a lista hidden_ids que retorna 28 lista, todas com 2 nomes dentro [[x,x], [x,x]...]
-
-    -TEMOS QUE
-
-    checar qual o maior, o result [0] e o result [1], se o result[0] for maior deve ser somado 1 as vitorias dos nomes na lista hidden_ids[0][0] e hidden ids[0][1]
-
-    depois -> result[2] > result[3] _ se sim -> adiciona 1 na vitoria do hidden ids[1][0] e hidden ids[1][1]
-    '''
+    
     for num in range(0,len(hidden_ids), 2):
         if results[num] == results[num+1]:
-            print("OI")
             continue
         elif results[num] > results[num+1]:
-            print("OLA")
             cursor.execute("SELECT nome FROM jogadores WHERE nome IN (?,?)", (hidden_ids[num][0], hidden_ids[num][1]))
             nomes = cursor.fetchall()
 
             cursor.execute("SELECT vitorias FROM jogadores WHERE nome IN (?,?)", (hidden_ids[num][0], hidden_ids[num][1]))
             vitorias = cursor.fetchall()
-            print(vitorias)
-
             for i in range(2):
                 database.execute("UPDATE jogadores SET vitorias = ? WHERE nome = ?", (1 + int(vitorias[i][0]), nomes[i][0]))
             database.commit()
@@ -127,7 +129,6 @@ def button_refresh():
 
             cursor.execute("SELECT vitorias FROM jogadores WHERE nome IN (?,?)", (hidden_ids[num + 1][0], hidden_ids[num + 1][1]))
             vitorias = cursor.fetchall()
-            print(vitorias)
 
             for i in range(2):
                 database.execute("UPDATE jogadores SET vitorias = ? WHERE nome = ?", (1 + int(vitorias[i][0]), nomes[i][0]))
@@ -142,7 +143,7 @@ def button_refresh():
     for ponto in pontos_on_data:
         pontos_list.append(int(ponto[0]))
 
-# ATUALIZA O BANCO DE DADOS COM A SOMA DAS PONTUACOES
+# ATUALIZA O BANCO DE DADOS jogadores COM A SOMA DAS PONTUACOES
     for num in range(28):
         cursor.execute("SELECT nome FROM jogadores WHERE nome IN (?,?)",(hidden_ids[num][0],hidden_ids[num][1]))
         nome = cursor.fetchall()
@@ -153,6 +154,21 @@ def button_refresh():
             database.execute("UPDATE jogadores SET pontos = ? WHERE nome = ?",(results[num] + int(ponto[i][0]), nome[i][0]))
 
         database.commit()
+
+# ATUALIZA O BANDO DE DADOS partidas COM OS RESULTADOS e jogo acontecido
+    for i in range (0,28,2):
+        
+        if results[i] or results[i+1] > 0:
+            database.execute("UPDATE partidas SET status = ? WHERE id = ?", ("True", str(i//2 +1)))
+
+        cursor.execute("SELECT result_time1, result_time2 FROM partidas WHERE id = ?", (str(i//2 + 1),))
+        resultado_aux = cursor.fetchall()[0]
+        if resultado_aux[0] == 0 and resultado_aux[1] == 0:
+            database.execute("UPDATE partidas SET result_time1 = ? WHERE id = ?", (results[i], str(i//2 + 1)))
+            database.execute("UPDATE partidas SET result_time2 = ? WHERE id = ?", (results[i+1], str(i//2 +1)))
+            database.execute("UPDATE partidas SET result_time2 = ? WHERE id = ?", (results[i+1], str(i//2 +1)))
+    database.commit()
+
 
 # COLOCA O RANK DO MAIOR PARA O MENOR
     rank_pontos = []
@@ -174,8 +190,21 @@ def button_refresh():
         rank_jogadores.append(dados_db[i][1])
         rank_vitorias.append(dados_db[i][3])
     
+# RETORNA O STATUS DOS JOGOS
+    cursor.execute("SELECT status FROM partidas")
+    status_on_data = cursor.fetchall()
+    status = []
+    for texts in status_on_data:
+        status.append(texts[0])
 
-    return render_template("torneio.html",rank_pontos = rank_pontos, player = player, rank_jogadores = rank_jogadores, rank_vitorias = rank_vitorias)
+# RETORNA O RESULTADO DE CADA JOGO [[x,x]...]
+    cursor.execute("SELECT result_time1, result_time2 FROM partidas")
+    results_on_data = [list(linha) for linha in cursor.fetchall()]
+
+
+
+
+    return render_template("torneio.html",rank_pontos = rank_pontos, player = player, rank_jogadores = rank_jogadores, rank_vitorias = rank_vitorias, status = status, results_on_data = results_on_data)
     
 
 
